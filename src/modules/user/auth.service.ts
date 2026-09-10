c8db78bdb539caa37db94f21a5694f8e26cdf43d
semanaparfait <p.semana@alustudent.com>
2026-09-10 21:22:55 +0200

    fix: handle unauthorized access for users without a company in login

--- src/modules/user/auth.service.ts
+++ src/modules/user/auth.service.ts
@@ -34,13 +34,15 @@ export class AuthService {
     if (user.role === UserRole.EMPLOYEE)
       throw new UnauthorizedException('Employees must login using PIN');
 
-    if (user.role === UserRole.OWNER) {
-      await this.deviceService.getOne({
-        deviceId: dto.deviceId,
-        registrationStatus: RegistrationStatusEnum.REGISTERED,
-        company: { id: user.company.id },
-      });
+    if (!user.company) {
+      throw new UnauthorizedException('User is not assigned to a company');
     }
+
+    await this.deviceService.getOne({
+      deviceId: dto.deviceId,
+      registrationStatus: RegistrationStatusEnum.REGISTERED,
+      company: { id: user.company.id },
+    });
 
     const validPassword = await compareHashContent(dto.password, user.password);
 
